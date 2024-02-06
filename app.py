72a22aefdb5886a016cbf1e552fa1850ca650140
Brandon <bclai27@gmail.com>
2024-02-06 16:55:29 -0500

    register view_count blueprint in main app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from top_articles import top_articles
 
 app = Flask(__name__)
 app.register_blueprint(top_articles)
+app.register_blueprint(view_count)
 
 app.config['HEADERS'] = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'}
 
