7cbcaaebc4cb7b2f9f92e06eb0b534c9eff2ffa1
Brandon <bclai27@gmail.com>
2024-02-06 16:55:01 -0500

    Update to add endpoint for getting view count of an article in a given week

--- view_count.py
+++ view_count.py
@@ -1,7 +1,7 @@
-from flask import make_response, request, jsonify, Blueprint, current_app
+from flask import request, Blueprint, current_app
 import requests
 from datetime import datetime, timedelta
-from utils import get_days_in_range, create_response_object
+from utils import create_response_object, get_week_start_and_end_dates
 import calendar
 
 view_count = Blueprint("view_count", __name__)
@@ -28,7 +28,14 @@ def get_view_count(title: str, domain: str):
             view_count_response = requests.get(request_url, headers=current_app.config['HEADERS'])
             response_obj = view_count_response.json()['items'][0]
             view_count_json = {'data': {'type': 'month', 'dates': [start_date.strftime("%B %Y")], 'article': response_obj['article']}, 'domain': response_obj['project'], 'views': response_obj['views']}
-            pass
+        elif year and month and day:
+            # Week
+            date = datetime(int(year), int(month), int(day))
+            week_start_date, week_end_date = get_week_start_and_end_dates(date)
+            if week_end_date >= datetime.today():
+                return create_response_object("We do not yet have data for this week", "get", 404, "Not Found")
+            
+            view_count_json = {'data': get_week_view_count(week_start_date, week_end_date, domain, title)}
         else:
             return create_response_object("Could not complete request. Please make sure your request is valid.", "get", 500, "Could not complete request")
         
@@ -37,3 +44,21 @@ def get_view_count(title: str, domain: str):
         return create_response_object("Invalid date entered", "get", 400, "Bad Request")
     except Exception:
         return create_response_object("Could not complete request at this time. Please make sure your request is valid or try again later.", "get", 500, "Could not complete request at this time")
+    
+def get_week_view_count(start_date: datetime, end_date: datetime, domain: str, title: str):
+    request_url = f'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/{domain}/all-access/all-agents/{title}/daily/{start_date.year}{start_date:%m}{start_date:%d}00/{end_date.year}{end_date:%m}{end_date:%d}00'
+    view_count_response = requests.get(request_url, headers=current_app.config['HEADERS'])
+    response_obj_list = view_count_response.json()['items']
+    view_count = 0
+    for item in response_obj_list:
+        view_count = view_count + item['views']
+    
+    data = {
+        'type': 'week',
+        'dates': [start_date, end_date],
+        'article': title,
+        'domain': domain,
+        'views': view_count
+    }
+
+    return data
