66186d6d182fbfbc793821962a214ff827a57584
Brandon <bclai27@gmail.com>
2024-02-06 15:30:16 -0500

    Update to add support for multiple domains
    -ie. en.wikipedia

--- top_articles.py
+++ top_articles.py
@@ -5,8 +5,8 @@ from utils import get_days_in_range, create_response_object
 
 top_articles = Blueprint("top_articles", __name__)
 
-@top_articles.route('/api/v1/articles/most_viewed', methods=['GET'])
-def get_top_articles():
+@top_articles.route('/api/v1/articles/most_viewed/<domain>', methods=['GET'])
+def get_top_articles(domain: str):
     # Get year, month, and day params
     year = request.args.get('year')
     month = request.args.get('month')
@@ -16,7 +16,7 @@ def get_top_articles():
         # Check if monthly or weekly
         if year and month and day is None:
             # Month
-            request_url = f'https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/{year}/{month}/all-days'
+            request_url = f'https://wikimedia.org/api/rest_v1/metrics/pageviews/top/{domain}/all-access/{year}/{month}/all-days'
             top_articles_response = requests.get(request_url, headers=current_app.config['HEADERS'])
             date = datetime(int(year), int(month), 1)
             first_day_of_this_month = datetime.today().replace(day=1)
@@ -26,7 +26,7 @@ def get_top_articles():
         elif year and month and day:
             # Week
             date = datetime(int(year), int(month), int(day))
-            top_articles_list, dates_list = get_week_top_articles(date)
+            top_articles_list, dates_list = get_week_top_articles(date, domain)
             if(len(top_articles_list) == 0):
                 return create_response_object("We do not yet have data for this week", "get", 404, "Not Found")
             top_articles_json = {'data': {'type': 'week', 'dates': dates_list, 'articles': top_articles_list}}
@@ -39,7 +39,7 @@ def get_top_articles():
     except Exception:
         return create_response_object("Could not complete request at this time. Please make sure your request is valid or try again later.", "get", 500, "Could not complete request at this time")
 
-def get_week_top_articles(date: datetime):
+def get_week_top_articles(date: datetime, domain: str):
     '''Get the top articles for a week. Takes in the date as an argument and returns the top articles for the week starting
     on the Monday before the given date throughout the Sunday after the given date'''
     # Get list of days in the week for the given date
@@ -52,7 +52,7 @@ def get_week_top_articles(date: datetime):
     article_views_dict = {}
     for day in days_list:
         # Get list of articles for this day
-        request_url = f'https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/{day.year}/{day:%m}/{day:%d}'
+        request_url = f'https://wikimedia.org/api/rest_v1/metrics/pageviews/top/{domain}/all-access/{day.year}/{day:%m}/{day:%d}'
         day_top_articles_response = requests.get(request_url, headers=current_app.config['HEADERS'])
         day_top_articles_json = day_top_articles_response.json()
         day_top_articles_list = day_top_articles_json['items'][0]['articles']
