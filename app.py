6b7e98d160c58cf0f9a9888820c0f9007e11e810
Brandon <bclai27@gmail.com>
2024-02-05 23:21:14 -0500

    Update app.py to start adding functionality for getting top articles for a week

--- app.py
+++ app.py
@@ -23,4 +23,32 @@ def get_top_articles():
     if year and month and day is None:
         request_url = f'https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikisource/all-access/{year}/{month}/all-days'
         top_articles = requests.get(request_url, headers=HEADERS)
+    elif year and month and day:
+        get_week_top_articles(year, month, day)
+        return 'in weekly'
+    
     return top_articles.json()
+
+def get_week_top_articles(year, month, day):
+    '''Get the top articles for a week. Takes in the year, month, and day as an argument and returns the top articles for the week starting
+    on the Sunday before the given date throughout the Saturday after the given date'''
+    days_list = get_weekdays(year, month, day)
+    print(days_list)
+
+def get_weekdays(year, month, day) -> list:
+    '''Get all of the dates of the week for a given date, from Sunday to Saturday'''
+    day = f'{month}/{day}/{year}'
+    dt = datetime.strptime(day, '%m/%d/%Y')
+    
+    # Get the dates that start and end the week
+    week_start_date = dt - timedelta(days=dt.weekday()+1)
+    week_end_date = week_start_date + timedelta(days=6)
+
+    # Get all of the dates for the week
+    delta = week_end_date - week_start_date
+    days_of_week = []
+    for i in range(delta.days + 1):
+        day = week_start_date + timedelta(days=i)
+        days_of_week.append(day)
+    
+    return days_of_week
